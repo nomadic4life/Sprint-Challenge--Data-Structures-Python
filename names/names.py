696a8f78ad5d674d328543bfe74711ec65908ec0
nomadic4life <orbison.dennis@gmail.com>
2019-07-26 09:14:49 -0700

    names

--- names/names.py
+++ names/names.py
@@ -1,5 +1,27 @@
 import time
 
+# original solution
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
+
+# first pass solution
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,13 +32,19 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+counts = dict()
+for elem in names_1:
+    if elem not in counts:
+        counts[elem] = 1
+        continue
+    counts[elem] += 1
+for elem in names_2:
+    if elem not in counts:
+        counts[elem] = 1
+        continue
+    counts[elem] += 1
+duplicates = [k for k, v in counts.items() if v > 1]
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
